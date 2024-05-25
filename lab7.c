03c2744a4caaadc5957021d7aa53de58f21a16fe
Foxy041 <151525554+Foxy041@users.noreply.github.com>
2024-05-25 14:52:32 +1000

    lab7.c

--- lab7.c
+++ lab7.c
@@ -3,7 +3,6 @@
 Ввести с консоли или из файла элементы одного массива и построить на их основе другой, упорядочив элементы по годам рождения.
 Вывести результат.*/
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -30,13 +29,17 @@ void sortbyyear(struct human arr[], int size) {
 }
 
 int main() {
-    int n;
-    printf("Введите размер массива: ");
-    scanf("%d", &n);
-
-    struct human *man1 = (struct human*)malloc(n * sizeof(struct human));
-
     FILE *file = fopen("input.txt", "r");
+    int n = 0;
+    char line[256];
+
+ 
+    while (fgets(line, sizeof(line), file)) {
+        n++;
+    }
+    
+    rewind(file);
+    struct human *man1 = (struct human*)malloc(n * sizeof(struct human));
 
     for (int i = 0; i < n; i++) {
         fscanf(file, "%s %s %d", man1[i].name, man1[i].surname, &man1[i].birth_year);
